955c364f997ef153767e441ccdfc93aca4f08611
wuyuanwuhui99 <275018723@qq.com>
2024-03-20 23:26:31 +0800

    音乐圈页面布局开发

--- entry/src/main/ets/theme/size.ts
+++ entry/src/main/ets/theme/size.ts
@@ -6,6 +6,7 @@ export const bigIconSize:number = 40
 export const normalFontSize:number = 15
 export const bigFontSize:number = 25
 export const blockBorderRaduis:number = 15
+export const smallAvaterSize:number = 30
 export const middleAvaterSize:number = 40
 export const bigAvaterSize:number = 80
 export const swiperHeight:number = 180
--- entry/src/main/ets/utils/common.ts
+++ entry/src/main/ets/utils/common.ts
@@ -1,1 +1,25 @@
 export const zerofull = value => value < 10 ? '0' + value : value + '';
+
+export const formatTime=(value:any):string=>{
+  var date =new Date(value);
+  var nowDate = new Date()
+  let diff = Math.ceil((nowDate.getTime()-date.getTime())/1000);
+  if(diff < 60){
+    return "刚刚"
+  }else if(diff < 60*60){
+    return Math.ceil(diff/60) + "分前"
+  }else if(diff < 60*60*24){
+    return Math.ceil(diff/(60*60))+"小时前"
+  }else if(diff < 60*60*24*30){
+    return Math.ceil(diff/(60*60*24))+"天前"
+  }else if(diff < 60*60*24*30*12){
+    return Math.ceil(diff/(60*60*24*30))+"个月前"
+  }
+  const year = zerofull(date.getFullYear());
+  const month = zerofull(date.getMonth()+1);
+  const dates = zerofull(date.getDate());
+  const hour = zerofull(date.getHours());
+  const minutes = zerofull(date.getMinutes());
+  const seconds = zerofull(date.getSeconds());
+  return `${year}-${month}-${dates} ${hour}:${minutes}:${seconds}`
+};
