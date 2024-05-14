62a635ecc92421a146dfa7db88b81ee17500b721
wuyuanwuhui99 <275018723@qq.com>
2024-05-15 00:00:23 +0800

    修改音乐圈评论和回复问题

--- entry/src/main/ets/config/constant.ts
+++ entry/src/main/ets/config/constant.ts
@@ -1,3 +1,3 @@
 // export const HOST:string = 'http://q6aiga.natappfree.cc';// 外网映射地址
-export const HOST:string = 'http://192.168.137.1:5001';// 外网映射地址
+export const HOST:string = 'http://192.168.0.5:5001';// 外网映射地址
 export const USER_DATA:string = 'USER_DATA';
