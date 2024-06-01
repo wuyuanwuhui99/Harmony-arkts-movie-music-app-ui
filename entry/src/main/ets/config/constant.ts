a8b4973f481929b061e49f07709b8292b3e37bbe
wuyuanwuhui99 <275018723@qq.com>
2024-06-01 20:19:02 +0800

    音乐播放器功能开发

--- entry/src/main/ets/config/constant.ts
+++ entry/src/main/ets/config/constant.ts
@@ -1,6 +1,7 @@
 // export const HOST:string = 'http://q6aiga.natappfree.cc';// 外网映射地址
 export const HOST: string = 'http://192.168.0.5:5001'; // 内网地址
 export const USER_DATA: string = 'USER_DATA';
+export const MUSIC_STORAGE = 'MUSIC_STORAGE';
 
 export const enum SocialEnum { // 社交类型
   MUSIC = "MUSIC",
--- entry/src/main/ets/utils/common.ts
+++ entry/src/main/ets/utils/common.ts
@@ -1,7 +1,9 @@
-export const zerofull = value => value < 10 ? '0' + value : value + '';
+export const zerofull=(value:number):string|number=>{
+  return value < 9 ? "0"+value:value
+}
 
-export const formatTime=(value:string):string=>{
-  var date =new Date(value.replace(/T/,' ').replace(/\..+/g,''));
+export const formatTime=(value:any):string=>{
+  var date =new Date(value);
   var nowDate = new Date()
   let diff = Math.ceil((nowDate.getTime()-date.getTime())/1000);
   if(diff < 60){
@@ -22,4 +24,12 @@ export const formatTime=(value:string):string=>{
   const minutes = zerofull(date.getMinutes());
   const seconds = zerofull(date.getSeconds());
   return `${year}-${month}-${dates} ${hour}:${minutes}:${seconds}`
+};
+
+export const formatSecond=(value:number,showHour:boolean = false):string => {
+  if(showHour){
+    return `${zerofull(Math.floor(value / (60 * 60)))}:${zerofull(Math.floor(value % (60 * 60) / 60))}:${zerofull(Math.floor(value % (60 * 60) % 60))}`
+  }else{
+    return `${zerofull(Math.floor(value / 60))}:${zerofull(Math.floor(value % 60))}`
+  }
 };
