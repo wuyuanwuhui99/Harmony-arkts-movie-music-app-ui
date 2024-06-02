fcfd05471ded30778c8db02d4c273e27bb45c19b
wuyuanwuhui99 <275018723@qq.com>
2024-06-02 13:06:14 +0800

    音乐播放界面功能开发

--- entry/src/main/ets/config/constant.ts
+++ entry/src/main/ets/config/constant.ts
@@ -1,7 +1,8 @@
 // export const HOST:string = 'http://q6aiga.natappfree.cc';// 外网映射地址
 export const HOST: string = 'http://192.168.0.5:5001'; // 内网地址
 export const USER_DATA: string = 'USER_DATA';
-export const MUSIC_STORAGE = 'MUSIC_STORAGE';
+export const MUSIC_PLAYER = 'MUSIC_PLAYER';// 音乐播放器
+export const MUSIC_MODEL = 'MUSIC_MODEL';// 音乐实例对象
 
 export const enum SocialEnum { // 社交类型
   MUSIC = "MUSIC",
