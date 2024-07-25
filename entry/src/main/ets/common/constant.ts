11425dfe2dce5797dfb2082f9e5fbcc437254f24
wuyuanwuhui99 <275018723@qq.com>
2024-07-25 23:42:17 +0800

    音乐分类列表功能开发

--- entry/src/main/ets/common/constant.ts
+++ entry/src/main/ets/common/constant.ts
@@ -1,8 +1,10 @@
 // export const HOST:string = 'http://254a2y1767.qicp.vip';// 外网映射地址，一般只有晚上才开机，开机之后才能使用，如需调试请联系
 export const HOST: string = 'http://192.168.0.5:5001'; // 内网地址
 export const USER_DATA: string = 'USER_DATA';
-export const MUSIC_PLAYER = 'MUSIC_PLAYER';// 音乐播放器
-export const MUSIC_MODEL = 'MUSIC_MODEL';// 音乐实例对象
-export const MUSIC_STORAGE = 'MUSIC_STORAGE';// 音乐仓库
-export const MUSIC_SEARCH = 'MUSIC_SEARCH';// 音乐搜查
-export const FAVORITE_MUSIC = 'FAVORITE_MUSIC';// 收藏音乐
+export const MUSIC_PLAYER: string = 'MUSIC_PLAYER';// 音乐播放器
+export const MUSIC_MODEL: string = 'MUSIC_MODEL';// 音乐实例对象
+export const MUSIC_STORAGE: string = 'MUSIC_STORAGE';// 音乐仓库
+export const MUSIC_SEARCH: string = 'MUSIC_SEARCH';// 音乐搜查
+export const FAVORITE_MUSIC: string = 'FAVORITE_MUSIC';// 收藏音乐
+export const PAGE_SIZE:number = 20;// 收藏音乐
+export const MAX_PAGE_SIZE:number = 20;// 每页最大数量
