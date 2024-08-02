d8b0aaf8ce9a7ae4df360656cff6c702a738f8f7
wuyuanwuhui99 <275018723@qq.com>
2024-08-02 23:51:57 +0800

    增加欢迎页，修改启动和登录问题

--- entry/src/main/ets/common/constant.ts
+++ entry/src/main/ets/common/constant.ts
@@ -1,5 +1,5 @@
 // export const HOST:string = 'http://254a2y1767.qicp.vip';// 外网映射地址，一般只有晚上才开机，开机之后才能使用，如需调试请联系
-export const HOST: string = 'http://192.168.0.5:5001'; // 内网地址
+export const HOST: string = 'http://192.168.0.3:5001'; // 内网地址
 export const USER_DATA: string = 'USER_DATA';
 export const MUSIC_PLAYER: string = 'MUSIC_PLAYER';// 音乐播放器
 export const MUSIC_MODEL: string = 'MUSIC_MODEL';// 音乐实例对象
