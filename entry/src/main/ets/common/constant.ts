f73bfa97d98b46bca62fb19416158e5c3fa2602d
wuyuanwuhui99 <275018723@qq.com>
2024-08-04 08:43:10 +0800

    欢迎页功能开发

--- entry/src/main/ets/common/constant.ts
+++ entry/src/main/ets/common/constant.ts
@@ -1,5 +1,5 @@
 // export const HOST:string = 'http://254a2y1767.qicp.vip';// 外网映射地址，一般只有晚上才开机，开机之后才能使用，如需调试请联系
-export const HOST: string = 'http://192.168.0.3:5001'; // 内网地址
+export const HOST: string = 'http://192.168.0.103:5001'; // 内网地址
 export const USER_DATA: string = 'USER_DATA';
 export const MUSIC_PLAYER: string = 'MUSIC_PLAYER';// 音乐播放器
 export const MUSIC_MODEL: string = 'MUSIC_MODEL';// 音乐实例对象
@@ -7,4 +7,4 @@ export const MUSIC_STORAGE: string = 'MUSIC_STORAGE';// 音乐仓库
 export const MUSIC_SEARCH: string = 'MUSIC_SEARCH';// 音乐搜查
 export const FAVORITE_MUSIC: string = 'FAVORITE_MUSIC';// 收藏音乐
 export const PAGE_SIZE:number = 20;// 收藏音乐
-export const MAX_PAGE_SIZE:number = 20;// 每页最大数量
+export const MAX_PAGE_SIZE:number = 500;// 每页最大数量
