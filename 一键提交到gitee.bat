950f3570e01c49d263d49c5a477e0d86df6e64e8
wuyuanwuhui99 <275018723@qq.com>
2024-08-29 23:29:35 +0800

    修改shell脚本

--- 一键提交到gitee.bat
+++ 一键提交到gitee.bat
@@ -1,3 +1,5 @@
+f:
+cd flutter/flutter-movie-app-ui
 git remote rm origin
 git remote add origin https://gitee.com/wuyuanwuhui99/Harmony-arkts-movie-music-app-ui.git
 git push origin main
--- 一键提交到github.bat
+++ 一键提交到github.bat
@@ -1,3 +1,5 @@
+f:
+cd Harmony/harmony-arkTs-movie-ui
 git remote rm origin
 git remote add origin https://github.com/wuyuanwuhui99/Harmony-arkts-movie-music-app-ui.git
 git push origin main
